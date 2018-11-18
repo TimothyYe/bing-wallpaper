696683215cc5437f81a8034468a11d78c823d2e4
TimothyYe <yexiaozhou2003@gmail.com>
2018-11-18 17:48:10 +0800

    add xml parser

--- handlers/wallpaper.go
+++ handlers/wallpaper.go
@@ -2,12 +2,32 @@ package handlers
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	bingURL = `https://www.bing.com`
+	bingAPI = `https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US`
+)
+
 func RootHandler(c *gin.Context) {
-	quality := c.DefaultQuery("quality", "high")
-	c.String(http.StatusOK, fmt.Sprintf("Quality: %s", quality))
+	// size := c.DefaultQuery("size", "1920")
+	// format := c.DefaultQuery("foramt", "json")
+
+	resp, err := http.Get(bingURL)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+	c.String(http.StatusOK, string(body))
 }
