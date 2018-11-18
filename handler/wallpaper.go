bd600d37dd3eabfb539a656cce7aae6bf1673216
Timothy Ye <yexiaozhou2003@gmail.com>
2018-11-18 22:36:27 +0800

    finish response parser

--- handler/wallpaper.go
+++ handler/wallpaper.go
@@ -17,37 +17,72 @@ const (
 	bingAPI = `https://www.bing.com/HPImageArchive.aspx?format=xml&idx=%s&n=1&mkt=en-US`
 )
 
+var (
+	resolutionMap map[string]string
+)
+
+func init() {
+	resolutionMap = map[string]string{}
+	resolutionMap["1366"] = "1366x768.jpg"
+	resolutionMap["1920"] = "1920x1080.jpg"
+}
+
+// RootHandler handles default API requests
 func RootHandler(c *gin.Context) {
-	// size := c.DefaultQuery("size", "1920")
-	// format := c.DefaultQuery("foramt", "json")
+	resolution := c.DefaultQuery("resolution", "1920")
+	format := c.DefaultQuery("format", "json")
 	index := c.DefaultQuery("index", "0")
 
+	// check index
 	_, err := strconv.Atoi(index)
 	if err != nil {
 		// input index is invalid
 		c.String(http.StatusInternalServerError, "the image index is invalid")
+		return
+	}
+
+	// check format
+	if format != "json" && format != "image" {
+		c.String(http.StatusInternalServerError, "format parameter is invalid, should be json or image")
+		return
+	}
+
+	// check resolution
+	if resolution != "1920" && resolution != "1366" {
+		c.String(http.StatusInternalServerError, "resolution parameter is invalid, should be 1920 or 1366")
+		return
 	}
 
 	resp, err := http.Get(fmt.Sprintf(bingAPI, index))
 	if err != nil {
 		c.String(http.StatusInternalServerError, "failed to request bing.com")
+		return
 	}
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "failed to parse request body from bing.com")
+		return
 	}
 
-	response, err := parseResponse(body)
+	response, err := parseResponse(body, resolution)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "failed to parse request body from bing.com")
+		return
 	}
 
+	if format == "image" {
+		// redirect to image URL
+		c.Redirect(http.StatusTemporaryRedirect, response.URL)
+		return
+	}
+
+	// render response as JSON
 	c.JSON(http.StatusOK, response)
 }
 
-func parseResponse(xmlInput []byte) (*model.Response, error) {
+func parseResponse(xmlInput []byte, resolution string) (*model.Response, error) {
 	doc := etree.NewDocument()
 	if err := doc.ReadFromBytes(xmlInput); err != nil {
 		return nil, err
@@ -59,6 +94,9 @@ func parseResponse(xmlInput []byte) (*model.Response, error) {
 	resp := &model.Response{}
 	resp.StartDate = imgElem.SelectElement("startdate").Text()
 	resp.EndDate = imgElem.SelectElement("enddate").Text()
-	resp.URL = fmt.Sprintf("%s_%s")
+	resp.URL = fmt.Sprintf("%s%s_%s", bingURL, imgElem.SelectElement("urlBase").Text(), resolutionMap[resolution])
+	resp.Copyright = imgElem.SelectElement("copyright").Text()
+	resp.CopyrightLink = imgElem.SelectElement("copyrightlink").Text()
+
 	return resp, nil
 }
