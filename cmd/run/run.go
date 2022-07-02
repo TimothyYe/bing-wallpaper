857c69c8c5a86eb6b977dbc47ee36c16f13b892b
Timothy <yexiaozhou2003@gmail.com>
2022-07-02 15:30:57 +0800

    Bypass CORS checking

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -2,6 +2,7 @@ package run
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/TimothyYe/bing-wallpaper/handler"
 
@@ -16,11 +17,32 @@ var Cmd = &cobra.Command{
 	Run:   run,
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func run(cmd *cobra.Command, args []string) {
 	router := gin.Default()
 
+	router.Use(CORSMiddleware())
 	router.GET("/", handler.RootHandler)
 
-	router.Run(":9000")
+	if err := router.Run(":9000"); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("API is running...")
 }
