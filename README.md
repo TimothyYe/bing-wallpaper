29af436d1aa2eeebfd6bdc03ce8b1f4c7a34b821
Timothy <yexiaozhou2003@gmail.com>
2024-08-04 11:51:35 +0800

    support more resolutions

--- README.md
+++ README.md
@@ -10,29 +10,48 @@ A RESTful API to fetch daily wallpaper from Bing.com
 
 ### Parameters
 
-* `resolution` The resolution of wallpaper image. `1920` is the default value, you can also use `1366` and `3840`(4K resolution).
 * `format` The response format, can be `json` or `image`. __If response format is set as `image`, you will be redirected to the wallpaper image directly__.
 * `image_format` The format of the wallpaper image, available values are `jpg` or `webp`. The default value is `jpg`.
 * `index` The index of wallpaper, starts from 0. By default, `0` means to get today's image, `1` means to get the image of yesterday, and so on. Or you can specify it as `random` to choose a random index between 0 and 7.
 * `mkt` The region parameter, the default value is `zh-CN`, you can also use `en-US`, `ja-JP`, `en-AU`, `en-GB`, `de-DE`, `en-NZ`, `en-CA`. You can also set it as `random` to choose the region randomly.
+* `resolution` The resolution of wallpaper image. `1920` is the default value, you can also use `1366` and `3840` or `UHD` (4K resolution).
+
+The available resolution options are listed below:
+```
+UHD
+1920x1200
+1920x1080
+1366x768
+1280x768
+1024x768
+800x600
+800x480
+768x1280 (Portrait mode)
+720x1280 (Portrait mode)
+640x480
+480x800 (Portrait mode)
+400x240
+320x240
+240x320 (Portrait mode)
+```
 
 ### Example
 
 * Request
 
 ```text
-https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=zh-CN
+https://bing.biturl.top/?resolution=UHD&format=json&index=0&mkt=zh-CN
 ```
 
 * Response
 
 ```json
 {
-  "start_date": "20181118",
-  "end_date": "20181119",
-  "url": "https://www.bing.com/az/hprichbg/rb/NarrowsZion_ZH-CN9686302838_1920x1080.jpg",
-  "copyright": "锡安国家公园内的维尔京河，美国犹他州 (© Justinreznick/Getty Images)",
-  "copyright_link": "http://www.bing.com/search?q=%E9%94%A1%E5%AE%89%E5%9B%BD%E5%AE%B6%E5%85%AC%E5%9B%AD\\u0026form=hpcapt\\u0026mkt=zh-cn"
+"start_date": "20240803",
+"end_date": "20240804",
+"url": "https://www.bing.com/th?id=OHR.ImpalaOxpecker_ZH-CN9652434873_UHD.jpg",
+"copyright": "黑斑羚和红嘴牛椋鸟，南非 (© Matrishva Vyas/Getty Images)",
+"copyright_link": "https://www.bing.com/search?q=%E5%8F%8B%E8%B0%8A%E6%97%A5&form=hpcapt&mkt=zh-cn"
 }
 ```
 
--- wallpaper.go
+++ wallpaper.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -19,9 +19,10 @@ const (
 )
 
 var (
-	Resolution map[string]string
-	Markets    map[string]bool
-	cache      *bigcache.BigCache
+	Resolution     map[string]string
+	FullResolution map[string]string
+	Markets        map[string]bool
+	cache          *bigcache.BigCache
 )
 
 func init() {
@@ -29,6 +30,24 @@ func init() {
 		"1366": "1366x768.jpg",
 		"1920": "1920x1080.jpg",
 		"3840": "UHD.jpg",
+	}
+
+	FullResolution = map[string]string{
+		"UHD":       "UHD.jpg",
+		"1920x1200": "1920x1200.jpg",
+		"1920x1080": "1920x1080.jpg",
+		"1366x768":  "1366x768.jpg",
+		"1280x768":  "1280x768.jpg",
+		"1024x768":  "1024x768.jpg",
+		"800x600":   "800x600.jpg",
+		"800x480":   "800x480.jpg",
+		"768x1280":  "768x1280.jpg",
+		"720x1280":  "720x1280.jpg",
+		"640x480":   "640x480.jpg",
+		"480x800":   "480x800.jpg",
+		"400x240":   "400x240.jpg",
+		"320x240":   "320x240.jpg",
+		"240x320":   "240x320.jpg",
 	}
 
 	Markets = map[string]bool{
@@ -68,8 +87,23 @@ func init() {
 // Get bing.com wallpaper from bing api
 func Get(index uint, market, resolution string) (*Response, error) {
 	if _, ok := Resolution[resolution]; !ok {
-		return nil, fmt.Errorf("resolution %s is not supported", resolution)
+		// get the full resolution
+		if _, ok := FullResolution[resolution]; !ok {
+			return nil, fmt.Errorf("resolution %s is not supported", resolution)
+		}
 	}
+
+	// fetch from the old resolution
+	if _, ok := Resolution[resolution]; ok {
+		resolution = Resolution[resolution]
+	}
+
+	// replace the resolution with the full resolution
+	if _, ok := FullResolution[resolution]; ok {
+		resolution = FullResolution[resolution]
+	}
+
+	fmt.Println("resolution: ", resolution)
 
 	if _, ok := Markets[market]; !ok {
 		return nil, fmt.Errorf("market %s is not supported", market)
@@ -99,7 +133,7 @@ func Get(index uint, market, resolution string) (*Response, error) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse request body from %s", bingURL)
 	}
@@ -115,7 +149,7 @@ func Get(index uint, market, resolution string) (*Response, error) {
 	response := &Response{
 		StartDate:     imgElem.SelectElement("startdate").Text(),
 		EndDate:       imgElem.SelectElement("enddate").Text(),
-		URL:           fmt.Sprintf("%s%s_%s", bingURL, imgElem.SelectElement("urlBase").Text(), Resolution[resolution]),
+		URL:           fmt.Sprintf("%s%s_%s", bingURL, imgElem.SelectElement("urlBase").Text(), resolution),
 		Copyright:     imgElem.SelectElement("copyright").Text(),
 		CopyrightLink: imgElem.SelectElement("copyrightlink").Text(),
 	}
